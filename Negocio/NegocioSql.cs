0debef434117a4fed6349a977b36afb8406eaea8
Titicc <83614737+Titicc@users.noreply.github.com>
2023-10-05 11:36:51 -0600

    Negocio

---
+++ Negocio/NegocioSql.cs
@@ -0,0 +1,36 @@
+﻿using Data.Data;
+using System.Xml.Linq;
+namespace Negocio
+
+{
+    public class NegocioSQL
+    {
+        public AccesoDatosSql _accesoDatosSQL = new AccesoDatosSql();
+        public NegocioSQL(AccesoDatosSql accesoDatosSQL)
+        {
+            _accesoDatosSQL = accesoDatosSQL;
+        }
+
+        #region Actors
+        //public bool AgregarActors(Actors P_Entidad)
+        //{
+        //    return _accesoDatosSQL.AgregarActors(P_Entidad);
+        //}
+        //public List<Actors> ConsultarActors(Actors P_Entidad)
+        //{
+        //    return _accesoDatosSQL.ConsultarActors(P_Entidad);
+        //}
+        //public bool ModificarActors(Actors P_Entidad)
+        //{
+        //    return _accesoDatosSQL.ModificarActors(P_Entidad);
+        //}
+        //public bool EliminarActors(Actors P_Entidad)
+        //{
+        //    return _accesoDatosSQL.EliminarActors(P_Entidad);
+        //}
+        #endregion
+
+    }
+
+
+}
